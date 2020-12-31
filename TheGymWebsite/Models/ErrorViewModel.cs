53182e8a5602077336327e4e2ff0dd9327e9e089
HussainSaleh <hussainalazzani@hotmail.com>
2020-12-31 21:49:11 +0000

    Add project files.

---
+++ TheGymWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheGymWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
